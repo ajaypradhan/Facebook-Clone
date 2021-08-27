9651d79379583afc1bedf56d105ddd0665eb75db
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-27 22:16:36 +0530

    feed and header ui done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import Header from './Header';
-
+import "./App.css";
+import Feed from "./Feed";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       {/* Header */}
-      <Header/>
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+        <Feed />
+      </div>
     </div>
   );
 }
---
+++ src/StoryReel.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Story from "./Story";
+import "./StoryReel.css";
+
+function StoryReel() {
+  return (
+    <div className="storyReel">
+      <Story
+        className="storyReel_story"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ2ZwTolN_nUPOSatBIRFIstB8KDMjBzmUvw&usqp=CAU"
+        profileSrc="https://lh3.googleusercontent.com/ogw/ADea4I6tiP6DIa7vSQ0nrQm4SuhvaDeKip4Stbs-pu_vK5E=s83-c-mo"
+        title="Aajay Pradhan"
+      />
+      <Story
+        className="storyReel_story"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ2ZwTolN_nUPOSatBIRFIstB8KDMjBzmUvw&usqp=CAU"
+        profileSrc="https://lh3.googleusercontent.com/ogw/ADea4I6tiP6DIa7vSQ0nrQm4SuhvaDeKip4Stbs-pu_vK5E=s83-c-mo"
+        title="Aajay Pradhan"
+      />
+      <Story
+        className="storyReel_story"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ2ZwTolN_nUPOSatBIRFIstB8KDMjBzmUvw&usqp=CAU"
+        profileSrc="https://lh3.googleusercontent.com/ogw/ADea4I6tiP6DIa7vSQ0nrQm4SuhvaDeKip4Stbs-pu_vK5E=s83-c-mo"
+        title="Aajay Pradhan"
+      />
+      <Story
+        className="storyReel_story"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ2ZwTolN_nUPOSatBIRFIstB8KDMjBzmUvw&usqp=CAU"
+        profileSrc="https://lh3.googleusercontent.com/ogw/ADea4I6tiP6DIa7vSQ0nrQm4SuhvaDeKip4Stbs-pu_vK5E=s83-c-mo"
+        title="Aajay Pradhan"
+      />
+    </div>
+  );
+}
+
+export default StoryReel;
